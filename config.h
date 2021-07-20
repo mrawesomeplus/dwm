33bae75a326cfa4b9f83767f2bbb583b85593bad
mrawesomeplus <kj@vnetwork,com>
2021-07-20 15:17:27 -0500

    Fixes

--- config.h
+++ config.h
@@ -34,7 +34,7 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ TERMCLASS, NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
@@ -64,7 +64,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "urxvt", NULL };
+static const char *termcmd[]  = { TERMINAL, NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function             argument */
@@ -81,7 +81,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,            {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,                {0} },
 	{ MODKEY,                       XK_Tab,    view,                {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,          {0} },
+	{ MODKEY,                       XK_q,      killclient,          {0} },
 	{ MODKEY,                       XK_t,      setlayout,           {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,           {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,           {.v = &layouts[2]} },
