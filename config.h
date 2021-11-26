60f9e4cd1e481c219e2532220ede8c56604afb5f
root <root@electro.vnetwork.com>
2021-11-26 09:46:33 -0600

    Fixes

--- config.h
+++ config.h
@@ -1,18 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 
 /* Constants */
-#define TERMINAL "st"
-#define TERMCLASS "st"
+#define TERMCLASS "urxvtc"
+#define TERMINAL "urxvtc"
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 13;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12", "fontawesome:size=12" };
-static const char dmenufont[]       = "monospace:size=11";
+static const char *fonts[]          = { "inconsolata:size=14:antialiasing=true", "fontawesome:size=14" };
+static const char dmenufont[]       = "inconsolata:size=14:antialiasing=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -65,13 +65,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { TERMINAL, NULL };
+//volume controls
 
 static Key keys[] = {
 	/* modifier                     key        function             argument */
 	{ MODKEY,                       XK_d,      spawn,               {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,               {.v = termcmd } },
-	{ MODKEY,           			XK_w,	   spawn,		        SHCMD("$BROWSER") },
-	{ MODKEY,		            	XK_e,	   spawn,	        	SHCMD(TERMINAL " -e mutt") },
+	{ MODKEY,           		XK_w,	   spawn,		SHCMD("$BROWSER") },
+	{ MODKEY,		        XK_e,	   spawn,	        SHCMD(TERMINAL " -e mutt") },
 	{ MODKEY,                       XK_b,      togglebar,           {0} },
 	{ MODKEY,                       XK_j,      focusstack,          {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,          {.i = -1 } },
@@ -84,7 +85,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_q,      killclient,          {0} },
 	{ MODKEY,                       XK_t,      setlayout,           {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,           {.v = &layouts[1]} },
-    { MODKEY|ShiftMask,             XK_f,      togglefullscr,       {0} },
+    	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,       {0} },
 	{ MODKEY,                       XK_m,      setlayout,           {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,           {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating,      {0} },
@@ -97,7 +98,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,             {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,             {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,             {.i = 0  } },
-    { MODKEY,                       XK_s,      spawn,               SHCMD("signal-desktop") },
+    	{ MODKEY,                       XK_s,      spawn,               SHCMD("signal-desktop") },
 	TAGKEYS(                        XK_1,                           0)
 	TAGKEYS(                        XK_2,                           1)
 	TAGKEYS(                        XK_3,                           2)
@@ -110,8 +111,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,                {0} },
 	/*volume binds */
 	{ MODKEY,			XK_F10,    spawn, 		SHCMD("pamixer -t") },
-	{ MODKEY, 			XK_F11,    spawn, 		SHCMD("pamixer --allow-boost -d 5") },
-	{ MODKEY,			XK_F12,    spawn, 		SHCMD("pamixer --allow-boost -i 5") },
+	{ MODKEY, 			XK_F11,    spawn, 		SHCMD("pamixer -d 5") },
+	{ MODKEY,			XK_F12,    spawn, 		SHCMD("pamixer -i 5")   },
 };
 
 /* button definitions */
