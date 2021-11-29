87728a364e9fbeeb7e468ee012f017e71a1c4cb1
mrawesomeplus <kj@vnetwork.com>
2021-11-28 19:59:47 -0600

    Fixes

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#ff6464";
+static const char col_cyan[]        = "#ff5555";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -70,7 +70,6 @@ static const char *termcmd[]  = { TERMINAL, NULL };
 static Key keys[] = {
 	/* modifier                     key        function             argument */
 	{ MODKEY,                       XK_d,      spawn,               {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,               {.v = termcmd } },
 	{ MODKEY,           		XK_w,	   spawn,		SHCMD("$BROWSER") },
 	{ MODKEY,		        XK_e,	   spawn,	        SHCMD(TERMINAL " -e mutt") },
 	{ MODKEY,                       XK_b,      togglebar,           {0} },
@@ -80,25 +79,28 @@ static Key keys[] = {
 	{ MODKEY,                       XK_i,      incnmaster,          {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,            {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,            {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_Return, zoom,                {0} },
-	{ MODKEY,                       XK_Tab,    view,                {0} },
+	{ MODKEY|ShiftMask,		XK_l,	   spawn,		SHCMD("slock") },
+    	{ MODKEY,                       XK_s,      spawn,               SHCMD("signal-desktop") },
 	{ MODKEY,                       XK_q,      killclient,          {0} },
+	{ MODKEY|ShiftMask,             XK_q,      quit,                {0} },
 	{ MODKEY,                       XK_t,      setlayout,           {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,           {.v = &layouts[1]} },
     	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,       {0} },
 	{ MODKEY,                       XK_m,      setlayout,           {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,           {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating,      {0} },
+	{ MODKEY,                       XK_Tab,    view,                {0} },
 	{ MODKEY,                       XK_0,      view,                {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,                 {.ui = ~0 } },
+	{ MODKEY,                       XK_Return, spawn,               {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,                {0} },
+	{ MODKEY,                       XK_space,  setlayout,           {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating,      {0} },
 	{ MODKEY,                       XK_comma,  focusmon,            {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,            {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,              {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,            {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,              {.i = +1 } },
 	{ MODKEY,                       XK_minus,  setgaps,             {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,             {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,             {.i = 0  } },
-    	{ MODKEY,                       XK_s,      spawn,               SHCMD("signal-desktop") },
 	TAGKEYS(                        XK_1,                           0)
 	TAGKEYS(                        XK_2,                           1)
 	TAGKEYS(                        XK_3,                           2)
@@ -108,7 +110,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                           6)
 	TAGKEYS(                        XK_8,                           7)
 	TAGKEYS(                        XK_9,                           8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,                {0} },
 	/*volume binds */
 	{ MODKEY,			XK_F10,    spawn, 		SHCMD("pamixer -t") },
 	{ MODKEY, 			XK_F11,    spawn, 		SHCMD("pamixer -d 5") },
